2051560a94c10e637534e65ec1f045b68660d4b7
Monesha-mone <moneshamanju@gmail.com>
2023-06-30 11:39:57 +0530

    ec2

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+#Creating EC2 instance
+provider "aws" {
+  region     = "us-east-1"
+}
+resource "aws_instance" "EC2J" {
+ ami = "ami-007855ac798b5175e"
+ instance_type = "t2.micro"
+ key_name = "N.virginia"
+ security_groups = [sg-0e82ea59b182012ca]
+ subnet_id = "subnet-0d1ed53e48b93edab"
+ associate_public_ip_address = "true"
+ tags = {
+  Name = "EC2J"
+ }
+}
+output "EC2J" {
+  value = aws_instance.EC2J.public_ip
+}
+output "EC2J_instance_id" {
+  value = aws_instance.EC2J.id
+}
